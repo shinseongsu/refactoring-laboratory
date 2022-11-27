e8fbfd7acd576f12dff0db0f5528ebf48d61063a
Shin seongsu <tlstjdtn321@naver.com>
2022-11-27 23:30:20 +0900

    feat: Echo Client 만들기

---
+++ src/main/java/com/van/client/EhcoClient.java
@@ -0,0 +1,37 @@
+package com.van.client;
+
+import com.van.client.handler.EchoClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class EhcoClient {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new EchoClientHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.connect("localhost", 8080).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
